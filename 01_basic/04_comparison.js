4e1f7f282348a1bd2e45a3a5dbc055e0e3eb1ef7
Shekhar Shakti <119730877+shekharshakti@users.noreply.github.com>
2023-12-23 15:31:42 +0000

    comperision

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,15 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2<=1);
+console.log(2!=1);
+
+console.log("2">1);
+console.log("02">1); 
+
+//avoid conversion like console.log(null>0) 
+//console.log(null>=0)
+//console.log(null==0) 
+
+
+//=== strictly check
