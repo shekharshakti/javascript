ba0fd77f2459f67921a7e18bd5173772877c9c9a
Shekhar Shakti <119730877+shekharshakti@users.noreply.github.com>
2023-12-23 08:29:50 +0000

    variable config done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,14 @@
+const accountID=12201439
+let accountEmail="shekharrajput7050@gmail.com"
+var accountpassword="12345"
+// accountID =2 not allowed 
+
+console.log(accountID);
+accountEmail="abc"
+accountpassword="456"
+console.table([accountID,accountEmail,accountpassword,])
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
