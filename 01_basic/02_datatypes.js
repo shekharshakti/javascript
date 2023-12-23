10730658464b7641798f080e0b3af7cd513ed34f
Shekhar Shakti <119730877+shekharshakti@users.noreply.github.com>
2023-12-23 09:26:34 +0000

    datatype

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";//treat all js as newer version
+// alert(3+3 ) we are using node js not the browser
+// code readbility should be high
+
+let name ='sekhar'
+let age =19
+let isLoggedin=false
+
+// primitive data typpe
+//number => 2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>stand alone value/representation of empty value 
+//undefined=> value is not defined 
+//symbol=> unique 
+
+console.log(typeof undefined);//undefined
+console.log(typeof null); //object
